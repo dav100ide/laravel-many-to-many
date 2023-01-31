bb00b880fd5917205d6c1d40460b0677860f40a9
dav100ide <oreglio.dev@gmail.com>
2023-01-31 15:39:36 +0100

    seedato technologies

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
@@ -17,6 +18,9 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        Schema::disableForeignKeyConstraints();
+        Project::truncate();
+        Schema::enableForeignKeyConstraints();
         for ($i = 0; $i < 10; $i++) {
             $new_project = new Project();
 
